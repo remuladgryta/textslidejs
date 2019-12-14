cf6964740104ece19497d3f2322c3a6dda7d1ba4
Remuladgryta <remuladgryta@gmail.com>
2019-12-15 00:58:51 +0100

    Persist textarea contents with LocalStorage.

--- textslide.js
+++ textslide.js
@@ -27,6 +27,7 @@ function textslide(){
 }
 window.onload = function(){
   let messages_ta = document.querySelector('#messages');
+  loadFromStorage(messages_ta);
   loadLines(messages_ta);
   textslide();
   messages_ta.addEventListener('input', e => loadLines(messages_ta));
@@ -37,9 +38,30 @@ function loadLines(textarea){
   for (i in lines){
     lines[i] = wrapLowercase(lines[i]);
   }
+  saveToStorage(textarea);
 }
 
 function wrapLowercase(text){
   pattern = /([^A-Z]+)/g; //Match all sequences of characters that are not uppercase.
   return text.replace(pattern, "<span class='lower'>$1</span>");
+}
+
+function loadFromStorage(textArea){
+  try{
+    let savedContent = window.localStorage.getItem("textslide");
+    if (savedContent){
+      textArea.value = savedContent;
+    }
+  }
+  catch(e){
+    console.exception("Failed to load textarea from LocalStorage", e);
+  }
+}
+
+function saveToStorage(textArea){
+  try{
+    window.localStorage.setItem("textslide", textArea.value);
+  }catch(e){
+    console.exception("Failed to save textarea to LocalStorage", e);
+  }
 }
